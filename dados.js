aaf4399145d3e4e69cc4aa6e0bfa80d85701c06f
Kraskin-Websites <kraskin17@outlook.com>
2025-10-08 19:03:04 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,364 @@
+var MENU = {
+    "acai-creme": [
+        {
+            "id": "creme-300",
+            "img": "./img/cardapio/creme/copo330.jpg",
+            "name": "Copo Açai 330ML",
+            "dsc": "Creme de Açaí 300ML",
+            "price": 25.00
+        },
+        {
+            "id": "creme-500",
+            "img": "./img/cardapio/creme/copo500.jpg",
+            "name": "Copo Açai  550ML",
+            "dsc": "Copo Açaí 500ML",
+            "price": 30.00
+        },
+
+        {
+            "id": "creme-700",
+            "img": "./img/cardapio/creme/combo.jpg",
+            "name": "Combo Duplo min.2und",
+            "dsc": "Dois copos de Açai 330ml",
+            "price": 18.00
+        },
+        
+        
+        {
+            "id": "creme-1l",
+            "img": "./img/cardapio/creme/combo.jpg",
+            "name":"Combo Duplo 500ml min.2und",
+            "dsc": "Dois copos de 500ml",
+            "price": 31.50
+        }
+
+    ],
+    "acai-vitamina": [
+        {
+            "id": "vitamina-300",
+            "img": "./img/cardapio/vitamina/vitamina.png",
+            "name": "Vitamina 300ML",
+            "dsc": "Vitamina de Açai 300ML",
+            "price": 8.00
+        },
+
+        {
+            "id": "vitamina-500",
+            "img": "./img/cardapio/vitamina/vitamina.png",
+            "name": "Vitamina 500ML",
+            "dsc": "Vitamina de Açai 500ML",
+            "price": 12.00
+        },
+
+        {
+            "id": "vitamina-700",
+            "img": "./img/cardapio/vitamina/vitamina.png",
+            "name": "Vitamina 700ML",
+            "dsc": "Vitamina de Açai 700ML",
+            "price": 15.00
+        }
+       
+    ],
+    "milk-shake": [
+        {
+            "id": "milkshake-300",
+            "img": "./img/cardapio/mikshake/milkshake.png",
+            "name": "Milk-Shake 300ML",
+            "dsc": "Milk-Shake 300ML",
+            "price": 9.00
+        },
+        {
+            "id": "milkshake-500",
+            "img": "./img/cardapio/mikshake/milkshake.png",
+            "name": "Milk-Shake 500ML",
+            "dsc": "Milk-Shake 500ML",
+            "price": 12.00
+        },
+        {
+            "id": "milkshake-700",
+            "img": "./img/cardapio/mikshake/milkshake.png",
+            "name": "Milk-Shake 700ML",
+            "dsc": "Milk-Shake 700ML",
+            "price": 15.00
+        }
+       
+    ]
+}
+
+var ACRESCIMOS = {
+    "acrescimos-comum": [
+        {
+            "id": "leite-em-po",
+            "name": "Leite em Pó ",
+            "price": 2.00
+        },
+        {
+            "id": "leite-condensado",
+            "name": "Leite Condensado",
+            "price": 2.00
+        },     
+        {
+            "id": "morango",
+            "name": "Morango",
+            "price": 5.50
+        },
+        {
+            "id": "pacoca",
+            "name": "Paçoca",
+            "price": 2.00
+        },
+        {
+            "id": "kiwi",
+            "name": "Pessego em calda",
+            "price": 2.00
+        },
+        {
+            "id": "banana",
+            "name": "Banana",
+            "price": 2.00
+        },
+        {
+            "id": "granola",
+            "name": "Granola",
+            "price": 2.00
+        },
+        {
+            "id": "manga",
+            "name": "Manga",
+            "price": 2.00
+        },
+        {
+            "id": "abacaxi",
+            "name": "Abacaxi",
+            "price": 2.00
+        },
+        
+    ],
+    "acrescimos-especiais": [
+        {
+            "id": "bis",
+            "name": "Bis",
+            "price": 1.50
+        },
+
+        {
+            "id": "nutella",
+            "name": "Cobertura de Nutella",
+            "price": 5.50
+        },
+        {
+            "id": "kit-kat",
+            "name": "Recheio Nutella",
+            "price": 11.00
+        },
+        {
+            "id": "creme-ninho",
+            "name": "Cobertura creme leitinho",
+            "price": 4.50
+        },
+        {
+            "id": "mouse-maracuja",
+            "name": "Recheio de creme leitinho",
+            "price": 8.00
+        },
+        {
+            "id": "gotas-chocolate",
+            "name": "Cobertura de creme Prestigio",
+            "price": 4.50
+        },
+        {
+            "id": "granulado",
+            "name": "Cobertura fini beijos",
+            "price": 9.00
+        },
+
+             {
+            "id": "granulado",
+            "name": "Cobertura fini dentaduras",
+            "price": 9.00
+        },
+
+             {
+            "id": "granulado",
+            "name": "Cobertura fini banana",
+            "price": 9.00
+        },
+
+        {
+            "id": "mEm",
+            "name": "M&M's",
+            "price": 5.00
+        },
+
+        {
+            "id": "ovomaltine",
+            "name": "Ovomaltine",
+            "price": 5.00
+        },
+
+                {
+            "id": "granulado",
+            "name": "Cobertura creme ovomaltine",
+            "price": 9.00
+        },
+
+
+        
+        {
+            "id": "chocoball",
+            "name": "Leite em Pó ninho",
+            "price": 4.00
+        }
+
+        
+    ]
+}
+
+var MILK_SHAKE = {
+    "sorvetes": [
+        {
+            "id": "chocolate",
+            "name": "Chocolate",
+            "desc": "sorvete"
+        },
+        {
+            "id": "morango",
+            "name": "Morango",
+            "desc": "sorvete"
+        },     
+        {
+            "id": "coco",
+            "name": "Coco",
+            "desc": "sorvete"
+        },
+        {
+            "id": "sensacao",
+            "name": "Sensação",
+            "desc": "sorvete"
+        },
+        {
+            "id": "amendoim",
+            "name": "Amendoim",
+            "desc": "sorvete"
+        },
+        {
+            "id": "bom-bom",
+            "name": "Bom-Bom",
+            "desc": "sorvete"
+        },
+        {
+            "id": "chicletes",
+            "name": "Chicletes",
+            "desc": "sorvete"
+        },
+        {
+            "id": "ninho",
+            "name": "Ninho",
+            "desc": "sorvete"
+        },
+        {
+            "id": "talento",
+            "name": "Talento",
+            "desc": "sorvete"
+        },
+
+        {
+            "id": "laka",
+            "name": "Laka",
+            "desc": "sorvete"
+        },
+        
+        {
+            "id": "limao",
+            "name": "Limão",
+            "desc": "sorvete"
+        },
+
+    ],
+    "coberturas": [
+        {
+            "id": "chocolate",
+            "name": "Chocolate",
+            "desc": "cobertura"
+        },
+        {
+            "id": "morango",
+            "name": "Morango",
+            "desc": "cobertura"
+        },     
+        {
+            "id": "leite-condensado",
+            "name": "Leite Condensado",
+            "desc": "cobertura"
+        },
+        {
+            "id": "doce-de-leite",
+            "name": "Doce de Leite",
+            "desc": "cobertura"
+        },
+        {
+            "id": "caramelo",
+            "name": "Caramelo",
+            "desc": "cobertura"
+        },
+        {
+            "id": "maracuja",
+            "name": "Maracujá",
+            "desc": "cobertura"
+        },
+        {
+            "id": "uva",
+            "name": "Uva",
+            "desc": "cobertura"
+        },
+
+    ]
+
+}
+
+// Remover comentario em produção
+
+function recarregarScript() {
+    // Obtenha o elemento script existente
+    var scriptElement = document.getElementById('dados');
+    var scriptElement2  = document.getElementById('appId');
+
+    if (scriptElement) {
+        // Remova o script do DOM
+        scriptElement.parentNode.removeChild(scriptElement);
+
+        // Crie um novo elemento script
+        var novoScript = document.createElement('script');
+
+        // Atribua o mesmo ID para garantir que o novo script seja reconhecido
+        novoScript.id = 'dados';
+
+        // Atribua o caminho do arquivo ao novo script
+        novoScript.src = './js/dados.js';
+
+        // Adicione o novo script ao final do corpo (ou ao local desejado)
+        document.body.appendChild(novoScript);
+        
+    } else {
+        //console.error('Script não encontrado.');
+    }
+}
+
+// Armazene o estado do console
+var consoleAberto = false;
+
+// Verifique regularmente se o console está aberto
+setInterval(function() {
+    if (window.outerWidth - window.innerWidth > 200 || window.outerHeight - window.innerHeight > 200) {
+        carregarDadosNovamente();
+
+    }
+}, 5000);
+
+function carregarDadosNovamente() {
+    MEU_CARRINHO = [];
+    localStorage.setItem('meu_carrinho', JSON.stringify([MEU_CARRINHO]));
+    recarregarScript();
+    cardapio.metodos.atualizarBadgeTotal();
+    //console.log('O console foi aberto..');
+}
